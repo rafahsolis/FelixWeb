65904b2d815a0de650975e0ad0dfbdb107c67be0
Rafael Herrero Solis <rafael@herrerosolis.com>
2018-12-16 11:58:24 +0100

    Wireframing

--- FelixWeb/settings.py
+++ FelixWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5+8ryaj3_k5$6)bit&6za&s!8e!2p31tauzj^uk4kup#v6!anb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'FelixWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
